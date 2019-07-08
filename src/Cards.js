325a4d50a28694dcb28faa4672922ea748961c02
Brendan Brown <pbbrown1112@gmail.com>
2019-07-08 16:08:05 -0500

    cards can turn over now

--- src/Cards.js
+++ src/Cards.js
@@ -1,8 +1,9 @@
 class Card {
     
-    constructor(name, image){
+    constructor(name, image, id){
         this.name = name;
         this.image = image;
+        this.id = id
 
         this.create_card()
 
@@ -12,16 +13,27 @@ class Card {
         let card_div = document.getElementById('card-area');
 
         let div = document.createElement('div');
-        div.setAttribute('class', 'col');
+        div.setAttribute('class', 'col-4');
+        div.setAttribute('id', this.id)
         let img = document.createElement('img');
-        img.src = this.image;
+        img.src = "https://previews.123rf.com/images/rlmf/rlmf1512/rlmf151200181/49319355-playing-cards-back.jpg";
         img.setAttribute('class', 'toy-avatar');
         div.append(img)
-
+        img.addEventListener('click', () => {
+            img.src = this.image
+            setTimeout( ()=>{
+                img.src = "https://previews.123rf.com/images/rlmf/rlmf1512/rlmf151200181/49319355-playing-cards-back.jpg"
+            }, 2000)
+            })         
+        
         card_div.append(div);
 
-
-
     }
+
+    // function turnCardOver() {
+    //     img.src = this.image
+    //         sleep 5000
+    //         img.src = "https://previews.123rf.com/images/rlmf/rlmf1512/rlmf151200181/49319355-playing-cards-back.jpg"
+    //     }
 
 }
