eeda011c169c39f8b4d08f06e592f69f432b66b1
Brendan Brown <pbbrown1112@gmail.com>
2019-07-10 14:26:12 -0500

    need to add functionality for matching cards

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-// Count total moves
-let total_moves = 0;
+    // Count total moves
+    let total_moves = 0;
 
-// Clicked cards array
-let clicked_cards = [];
+    // Clicked cards array
+    let clicked_cards = [];
 
-    
 
-// Points to Win
-const points_to_win = document.querySelectorAll('img.toy-avatar').length
+        // Points to Win
+        // const points_to_win = document.querySelectorAll('img.toy-avatar').length
 
-let points = 0;
+        // let points = 0;
+
+        // if (points == ) {
+        //     alert('You Win!')
+        // }
+
