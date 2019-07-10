bf1cb92683fa96fcfe813781665edf6c57f38e72
Maruf <marufbinawal@gmail.com>
2019-07-10 11:49:38 -0500

    added schema, modified gemfile to add rackCORS

--- game_backend/app/controllers/games_controller.rb
+++ game_backend/app/controllers/games_controller.rb
@@ -1,2 +1,22 @@
 class GamesController < ApplicationController
+    def show
+        game = Game.find(params[:id])
+        render json: game
+    end
+
+    def index
+        game = Game.all
+        render json: game
+    end
+
+    def update
+        # Locates current game
+        # Save params in the database -> Update the User info
+        game = Game.find(params[:id])
+        game.update(game_params)
+    end
+
+    def game_params
+        params.permit( :time, :score, :user_id )
+    end
 end
