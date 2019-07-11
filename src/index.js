701c7a2d7cf2ff0cd4d9b4f10131ee8b8080b5b3
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-07-11 14:03:38 -0500

    testing2

--- src/index.js
+++ src/index.js
@@ -6,20 +6,9 @@
 
     let userName;
 
-<<<<<<< HEAD
     // Points to Win
     // const points_to_win = document.querySelectorAll('img.toy-avatar').length
     let timer;
     let points = 0;
 
-=======
-        // Points to Win
-        const points_to_win = document.querySelectorAll('img.toy-avatar').length
-
-        let points = 0;
-
-        // if (points == points_to_win) {
-        //     alert('You Win!')
-        // }
->>>>>>> b9679f6c92c68154f8b438d76d744f51f7e2527c
 
